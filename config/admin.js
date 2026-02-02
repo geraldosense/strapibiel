be71ff767aab29e82b4ba7c8b4ecab9a24aa881c
geraldosense <sensegeraldo2@gmail.com>
2026-02-02 11:39:03 +0000

    Complete Railway configuration with PostgreSQL and all config files

---
+++ config/admin.js
@@ -0,0 +1,10 @@
+﻿module.exports = ({ env }) => ({
+  auth: {
+    secret: env('ADMIN_JWT_SECRET'),
+  },
+  apiToken: {
+    salt: env('API_TOKEN_SALT'),
+  },
+  url: '/admin',
+  autoOpen: false,
+});
